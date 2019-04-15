e4b3d1854a37a9c4af7dffe845ed47c5d693da66
zhugaomeng <974203736@qq.com>
2019-04-15 19:59:17 +0800

    全部版

--- Manager/src/router/modules/questions.js
+++ Manager/src/router/modules/questions.js
@@ -3,34 +3,41 @@
 import Layout from '@/layout'
 
 const questionsRouter = {
-  path: '/questions',
-  component: Layout,
-  redirect: 'noredirect',
-  name: 'Questions',
-  meta: {
-    title: 'questions',
-    icon: 'clipboard'
-  },
-  children: [
-    {
-      path: 'addItem',
-      component: () => import('@/views/Questions/addItems'),
-      name: 'KeyboardChart',
-      meta: { title: 'addItem', noCache: true }
+    path: '/questions',
+    component: Layout,
+    redirect: 'noredirect',
+    name: 'Questions',
+    meta: {
+        title: 'questions',
+        icon: 'clipboard'
     },
-    {
-      path: 'questionClassification',
-      component: () => import('@/views/Questions/addlist'),
-      name: 'LineChart',
-      meta: { title: 'questionClassification', noCache: true }
-    },
-    {
-      path: 'checkTheItem',
-      component: () => import('@/views/charts/mixChart'),
-      name: 'MixChart',
-      meta: { title: 'checkTheItem', noCache: true }
-    }
-  ]
+    children: [{
+            path: 'addItem',
+            component: () =>
+                import ('@/views/Questions/addItems'),
+            name: 'KeyboardChart',
+            meta: { title: 'addItem', noCache: true }
+        },
+        {
+            path: 'questionClassification',
+            component: () =>
+                import ('@/views/Questions/addclassify'),
+            name: 'LineChart',
+            meta: { title: 'questionClassification', noCache: true }
+        },
+        {
+            path: 'checkTheItem',
+            component: () =>
+                import ('@/views/Questions/addlist'),
+            name: 'MixChart',
+            meta: { title: 'checkTheItem', noCache: true }
+        }, {
+            path: 'detial',
+            component: () =>
+                import ('@/views/Questions/detial'),
+            meta: { title: '', noCache: true }
+        }
+    ]
 }
 
 export default questionsRouter
