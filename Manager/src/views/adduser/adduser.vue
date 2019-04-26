a3ae65c9b99a4a664f246d98b634d6cd7af3bfa5
Heyunzheng <2440926502@qq.com>
2019-04-26 15:56:36 +0800

    修改bug

--- Manager/src/views/adduser/adduser.vue
+++ Manager/src/views/adduser/adduser.vue
@@ -176,11 +176,15 @@ export default {
       this.view_select = ''
     },
     adduser() { // 添加用户的逻辑
+      if(!this.userData[this.select]){
+        this.$message.error('请检查未填写值')
+      }else{
+         this.addUserValue = this.userData[this.select]
+      }
       var uPattern = /^[a-zA-Z0-9_-]{4,16}$/
       var Reg = /^.*(?=.{6,})(?=.*\d)(?=.*[A-Z])(?=.*[a-z])(?=.*[!@#$%^&*? ]).*$/
       var blone = Reg.test(this.pwd)
       var userblone = uPattern.test(this.name)
-      this.addUserValue = this.userData[this.select]
       if (this.name === '' || this.addUserValue === null || this.pwd === '') {
         this.$message.error('请检查未填写值')
       } else if (blone === false) {
@@ -235,7 +239,6 @@ export default {
         this.$message.error('请检查未填写值')
       } else {
         this.addusercrad({ identity_text: this.person }).then(() => {
-          console.log(this.adduserCode)
           if (this.adduserCode === 1) {
             this.$message({ message: '恭喜你，添加成功', type: 'success' })
             this.userdata()
